28eb2cea933d92034f28fab215b22167a44bb7bb
tintin-zheng <bowen2.24@intl.zju.edu.cn>
2025-03-12 08:06:56 +0100

    change  of  the truth  table

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -18,7 +18,25 @@ else:
 #drive time is longer
 
 #creat the variable X and Y
-X = True
-Y = False
-W = X and Y
+#truth table for X and Y
+X_1 = True
+Y_1 = False
+W_1 = X_1 and Y_1
+X_2 = False
+Y_2 = True
+W_2 = X_2 and Y_2
+X_3 = True
+Y_3 = True
+W_3 = X_3 and Y_3
+X_4 = False
+Y_4 = False
+W_4 = X_4 and Y_4
+print(f"X-{X_1} and Y-{Y_1} is W-{W_1}",
+      f"X-{X_2} and Y-{Y_2} is W-{W_2}",
+      f"X-{X_3} and Y-{Y_3} is W-{W_3}",
+      f"X-{X_4} and Y-{Y_4} is W-{W_4}", sep = "\n")
+      
 #the truth table for W
+"""
+
+"""
