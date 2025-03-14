123d1e51c912aabda8c8eee4a40a1a8cf56b4efc
tintin-zheng <bowen2.24@intl.zju.edu.cn>
2025-03-14 03:29:44 +0100

    more commit

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -1,6 +1,7 @@
 #assign a value to a
 a = 0
-#creat a loop
+#creat a loop，to present the triangle from T1 to T10
 for i in range (1,11) :
+    #add the value of i to a
     a += i
     print(f"The value for the T{i} is {a}")
