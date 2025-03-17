beadac2103273ba70d25accc87d283a78677f07c
tintin-zheng <bowen2.24@intl.zju.edu.cn>
2025-03-17 09:24:54 +0100

    upload the Programming_language_poularity

---
+++ Practical5/Programming_language_popularity.py
@@ -0,0 +1,31 @@
+#import the matplotlib library for a bar plot plotting
+import matplotlib.pyplot as plt
+
+#creat a dictionary to store the data about the popularity of programming languages
+popularity = { "JavaScript" : 62.3,
+              "HTML": 52.9,
+              "python": 51, 
+              "SQL": 51,
+              "TypeScript": 38.5,
+              }
+
+#print the dictionary
+print(popularity)
+
+#creat the bar plot
+#import the data from the dictionary and choose a color for the bars
+plt.bar(popularity.keys(), popularity.values(), color = "#81D8CF")
+#add the title and labels to the plot
+plt.xlabel("Programming Languages")
+plt.ylabel("Popularity (%)")
+plt.title("Programming Language Popularity")
+#add the data on the top of the bars
+for key, value in popularity.items():
+    plt.text(key, value, str(value))
+#show the plot
+plt.show()
+
+#choose a language to check
+language = "python"
+#print the percentage of the users of the chosen language
+print(f"The percentage of the users of {language} is {popularity[language]}.")
