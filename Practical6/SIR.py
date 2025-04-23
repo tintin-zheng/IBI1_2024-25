de8f237d0dee0d6a4b7b37cc390010b92b531c99
Bowen Zheng <bowen2.24@intl.zju.edu.cn>
2025-04-23 15:50:08 +0800

    add some comments

--- Practical6/SIR.py
+++ Practical6/SIR.py
@@ -8,41 +8,45 @@
     then, store the data in the arrays S, I, and R.
     finally, plot the data.
 '''
-#import nessary libraries
+# import nessary libraries
 import numpy as np
 import matplotlib.pyplot as plt
-#define the variables
+# define the variables
 N = 10000
 beta = 0.3
 gamma = 0.05
 I0 = 1
 R0 = 0
 S0 = N - I0 - R0
-#creat the arrays to store the data
+# creat the arrays to store the data
 S = [S0]
 I = [I0]
 R = [R0]
-#creat the loop to calculate the data
+# creat the loop to calculate the data
 for i in range(1,1001):
-    #calculate the number of infected and recovered people
+    # calculate the number of infected and recovered people
+    # set the rate of infection and recovery
     Infected = beta * (I[-1]/N)
     Recovered = gamma
+    # set the number of new infected and recovered people to 0
     new_infected_people = 0
     new_recovered_people = 0
+    # create a random array to simulate the infection and recovery process
+    # in array, 0 means not infected and 1 means infected
     new_infected_people_array = np.random.choice(range(2), S[-1], p = [1-Infected, Infected]) 
     for i in new_infected_people_array.tolist():
         new_infected_people += i
     new_recovered_people_array = np.random.choice(range(2), I[-1], p = [1-Recovered, Recovered])
     for i in new_recovered_people_array.tolist():
         new_recovered_people += i
-    #store the data (update)
+    # store the data (update)
     s_new = S[-1] - new_infected_people
     i_new = I[-1] + new_infected_people - new_recovered_people
     r_new = R[-1] + new_recovered_people
     S.append(s_new)
     I.append(i_new)
     R.append(r_new)
-#plot the data
+# plot the data
 plt.plot(range(1001), S, color = "red", label = "Susceptible")
 plt.plot(range(1001), I, color = "darkblue", label = "Infected")
 plt.plot(range(1001), R, color = "green", label = "Recovered")
--- Practical6/SIR_vaccination.py
+++ Practical6/SIR_vaccination.py
@@ -9,30 +9,34 @@
     then, store the data in the arrays S, I, and R.
     finally, plot the data.
 '''
-#import nessary libraries
+# import nessary libraries
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 colors = cm.viridis(np.linspace(0, 1, 11))
-#define the variables
+# define the variables
 N = 10000
 beta = 0.3
 gamma = 0.05
-#add the vaccination process
+# add the vaccination process
 for idx,vaccinated_rate in enumerate(np.arange (0, 1.1, 0.1)):
     R0 = int(N * vaccinated_rate)
     I0 = 1
     S0 = max(N - I0 - R0,0)
-    #creat the arrays to store the data
+    # creat the arrays to store the data
     S = [S0]
     I = [I0]
     R = [R0]
-    #creat the loop to calculate the data
+    # creat the loop to calculate the data
     for i in range(1,1001):
+        # set the rate of infection and recovery
         Infected = beta * (I[-1]/N)
         Recovered = gamma
+        # set the number of new infected and recovered people to 0
         new_infected_people = 0
         new_recovered_people = 0
+        # create a random array to simulate the infection and recovery process
+        # in array, 0 means not infected and 1 means infected
         new_infected_people_array = np.random.choice(range(2), S[-1], p = [1-Infected, Infected]) 
         for i in new_infected_people_array.tolist():
             new_infected_people += i
